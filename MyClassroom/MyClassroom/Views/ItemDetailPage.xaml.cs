3b9a79a0bd59fcb181033f2fc70e3f817ce4cade
Achindra Bhatnagar <achindra@github.com>
2019-05-05 11:28:36 +0530

    MyClassroom App

---
+++ MyClassroom/MyClassroom/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using MyClassroom.ViewModels;
+
+using Xamarin.Forms;
+
+namespace MyClassroom.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
